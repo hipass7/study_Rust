33500552e4aaeda9a4d7b44564e5ae3bfaaaedd7
Hipass7 <qjsro8763@daum.net>
2024-07-22 16:47:34 +0900

    [240722] data type for decimal, boolean, character

--- 05-data_type/scalar.rs
+++ 05-data_type/scalar.rs
@@ -5,4 +5,14 @@ fn main() {
     let a = 0o15;
     let a =  0b1111_1111;
     let a: u8 = b'A';
+
+    // decimal
+    let a: f32  = 3.14;   // f32
+    let b = 3.14;         // f64
+
+    // boolean
+    let a: bool = false;
+
+    // character
+    let a: char = 'A';
 }
