efdedaa6d81d27fa013258bbcc4d0b1f213b2155
Hipass7 <qjsro8763@daum.net>
2024-07-23 16:48:40 +0900

    [230723] data type for array

---
+++ 05-data_type/compound.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    // array
+    let arr: [i32; 3] = [1, 2, 3];
+ 
+    println!("{}", arr[0]); // output : 1st data
+    println!("{:?}", arr); // output : entire array (included [])
+}
