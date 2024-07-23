9b19cb8d3e56525928ecd7a025d5a463a52ad14c
Hipass7 <qjsro8763@daum.net>
2024-07-23 16:36:03 +0900

    [240723] calculator for the four fundamental arithmetic operations

---
+++ 05-data_type/calculation.rs
@@ -0,0 +1,12 @@
+fn main()
+{
+    let a: i32 = 33;
+    let b: i32 = 1_000;
+    
+    println!("{} {} {} {}", a + b, a - b, a * b, b / a);
+
+    let pi: f32 = 3.141592;
+    let r: f32 = 2.5;
+    
+    println!("{}", pi * r);
+}
