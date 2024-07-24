013bd6d6fcb5a2073aa2cfecdd8ac37c0f05fc72
hipass7 <64785177+hipass7@users.noreply.github.com>
2024-07-24 21:42:17 +0900

    [230724] data type for tuple

--- 05-data_type/compound.rs
+++ 05-data_type/compound.rs
@@ -5,4 +5,16 @@ fn main()
  
     println!("{}", arr[0]); // output : 1st data
     println!("{:?}", arr); // output : entire array (included [])
+
+    // tuple
+    let dat: (i32, char, bool) = (1, 'A', true);
+    let usr = ("Tom", 'B');
+
+    let a = dat.0;
+    let b: char = dat.1;
+    let c: bool = dat.2;
+
+    // destructuring
+    let (a, b, c) = dat;
+    println!("{:?}", dat);
 }
