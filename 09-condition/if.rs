7b669212380d8b73780b74b0b4acebe2616d1af5
hipass7 <64785177+hipass7@users.noreply.github.com>
2024-08-04 23:48:20 +0900

    [240804] how to use conditional (if-else)

---
+++ 09-condition/if.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let score = 'C';
+ 
+    if score <= 'B' {
+        println!("Excellent");            
+    }    
+    else if score <= 'C' {
+        println!("Pass");            
+    }
+    else {
+        println!("Fail");            
+    }
+}
