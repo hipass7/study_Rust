7874f6abde700cdb06379018b5818befa48b829e
hipass7 <64785177+hipass7@users.noreply.github.com>
2024-08-04 23:43:23 +0900

    [240804] add 6 examples for expression

--- 08-expression/expression.rs
+++ 08-expression/expression.rs
@@ -1,21 +1,36 @@
 fn main() {
+    let number = 13;
+    
+    // 1. Concise and clear code
+    let odd = if number % 2 == 0 { "even" } else { "odd" };
+    println!("{}: {}", number, odd);
+
+    // 2. Immutability and functional programming style
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    // 3. Error prevention
+    fn add_one(x: i32) -> i32 {
+        x + 1  // No semicolon, so this value is returned
+    }
+
+    // 4. Code reuse and abstraction
     let c = add(1, 2);
     println!("{}", c);
 
-    let number = 13;
-    let odd = if number % 2 == 0 { "even" } else { "odd" };
-    
-    println!("{}: {}", number, odd);
+    // 5. Consistency
+    let result = if number % 2 == 0 {
+        42
+    } else {
+        0
+    };
+
+    // 6. Flexible control flow
 }
  
 fn add(a: i32, b: i32) -> i32 {
     dbg!(a, b);
     a + b // expression
 }
-
-// 1. Concise and clear code
-// 2. Immutability and functional programming style
-// 3. Error prevention
-// 4. Code reuse and abstraction
-// 5. Consistency
-// 6. Flexible control flow
