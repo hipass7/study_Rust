02dddb0e863cf48a62166316ce300c9e78ac934c
hipass7 <64785177+hipass7@users.noreply.github.com>
2024-09-05 22:56:44 +0900

    [240905] enum can assign the data to each type

--- 13-enum/enum.rs
+++ 13-enum/enum.rs
@@ -4,8 +4,21 @@ enum Priority {
     Medium,
     High
 }
+
+// data can be assigned to each type.
+#[derive(Debug)]
+enum LogType {
+    Info(String),      //(infoMsg)
+    Warning(String),   //(warningMsg)
+    Error(i32, String) //(errorCode, errorMsg)
+}
  
 fn main() {
     let urgent = Priority::High;
     dbg!(urgent);
+
+    let log = LogType::Error(1201, String::from("Not found"));
+    println!("{:?}", log);
+
+
 }
